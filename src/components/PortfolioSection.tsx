09c58747bd0f4b172a62154720132d5ee336d0c9
Jomin Binny <jominbinny21@gmail.com>
2025-05-13 12:26:18 +0530

    added

--- src/components/PortfolioSection.tsx
+++ src/components/PortfolioSection.tsx
@@ -20,7 +20,7 @@ const projects = [
     tags: ['JavaScript', 'React', 'Google Maps API'],
     image: 'https://images.unsplash.com/photo-1501785888041-af3ef285b470?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80',
     githubUrl: 'https://github.com/AmarnathSooraj/CalGo',
-    demoUrl: 'calgo.vercel.app'
+    demoUrl: 'https://calgo.vercel.app/'
   },
   {
     id: 3,
